211b8b6aa1ba88d7e728a4fbee57509ce64e515f
karengrisales <yine_net@hotmail.com>
2020-09-19 18:51:53 -0500

    Proyecto funciona con Sockets

---
+++ sockets/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+
+const http = require('http');
+const server = http.createServer(app);
+
+server.listen(3000);
+
+app.use(express.static('public'));
+
+const socketIo = require('socket.io');
+const io = socketIo.listen(server);
+
+io.on('connect', function (socket) {
+    console.log('nueva conexion id:' + socket.id);
+});
